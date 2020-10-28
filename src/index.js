f55f29954e761cf33c400f2379023f6b72edb4b5
Victoria Mount <victoriamount01@gmail.com>
2020-10-28 17:58:52 -0500

    Complete MVP

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-
+import { useDarkMode } from './hooks/useDarkMode'
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
 import "./styles.scss";
 
+const initialValues = {
+  darkOn: false
+}
+
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(initialValues);
 
   useEffect(() => {
     axios
